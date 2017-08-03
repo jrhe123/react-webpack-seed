85d25452ee4ecada673d5f8bd9a317020ab125ec
jrhe123 <keith_he@outlook.com>
2017-08-03 14:47:36 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+	entry: {
+		app: './src/app.js'
+	},
+	output: {
+		filename: 'build/bundle.js',
+		sourceMapFilename: 'build/bundle.map'
+	},
+	devtool: '#source-map',
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM'
+	},
+	module: {
+		loaders: [
+			{
+				loader:'babel',
+				exclude: /(node_modules)/,
+				query: {
+					presets: ['react', 'es2015']
+				}
+
+			}
+		]
+	}
+}
